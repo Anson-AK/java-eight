60a176216342a7819fbf6904a818b6bb9578edf2
Anson-AK <ansonak07@gmail.com>
2024-10-16 16:13:50 +0530

    Update Biconsumer.java

--- Java eight/functionalInteface/Biconsumer.java
+++ Java eight/functionalInteface/Biconsumer.java
@@ -7,7 +7,7 @@ public class Biconsumer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        BiConsumer<String,String> bc=(str,s)->System.out.println(str+s);
-       bc.accept("United"," Kingdom");
+       bc.accept("United"," States");
 	}
 
 }
